ef1a17a85ba27019a8689b1a66492e3edbe8dbf5
Takeaki <takeaki.yamamoto@gmail.com>
2026-04-11 22:23:54 -0400

    Delete rds related files

--- backend/src/repositories/postgres/index.ts
+++
@@ -1,9 +0,0 @@
-import { PgTripRepository } from './pgTripRepo';
-import { PgMemberRepository } from './pgMemberRepo';
-import { PgExpenseRepository } from './pgExpenseRepo';
-import { PgActivityRepository } from './pgActivityRepo';
-
-export const tripRepo = new PgTripRepository();
-export const memberRepo = new PgMemberRepository();
-export const expenseRepo = new PgExpenseRepository();
-export const activityRepo = new PgActivityRepository();
